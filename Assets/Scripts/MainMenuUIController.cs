daf394ebac17becd1175b02ab3c114326f4a2cf3
AH-work000 <70098254+AH-work000@users.noreply.github.com>
2021-12-16 16:21:30 +1100

    Flashing-Press-Start-Message

--- Assets/Scripts/MainMenuUIController.cs
+++ Assets/Scripts/MainMenuUIController.cs
@@ -1,19 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenuUIController : MonoBehaviour
 {
+
+    // Member Variables for the Red Text of the Canvas
+    [SerializeField]
+    private Text pressStartText;
+
+
+    // Member Variables for the timer
+    private float timer;
+
+
+    // Member Variables for the bool
+    private bool isPressStartTextEmpty;
+
+    private bool isPressStartTextThere;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Make isPressStartTextEmpty as false as the text concern
+        // is going to be there when the menu scene is rendered
+        isPressStartTextEmpty = false;
+
+        // Make isPressStartTextThere as true as the text concern
+        // is going to be there when the menu scene is rendered
+        isPressStartTextThere = true;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Timer count
+        timer += Time.deltaTime;
+
         // When the 'X' key is pressed, load the game scene
         if (Input.GetKeyDown(KeyCode.X))
         {
@@ -22,8 +49,45 @@ public class MainMenuUIController : MonoBehaviour
             //  UNCOMMENT THE BELOW CODE ONCE THE GAME SCENE IS FINALISED
             // SceneManager.LoadSceneAsync(1);
         }
-        
+
+
+        // Check if it have been 0.8 seconds and the Press Start Text Field is Filled in
+        if (isPressStartTextThere && timer >= 0.8f)
+        {
+            pressStartText.text = " ";
+
+            // Make the isPressStartTextEmpty text to be true
+            isPressStartTextEmpty = true;
+
+            // Make the isPressStartTextThere text to be false
+            isPressStartTextThere = false;
+
+            ResetTimer();
+        }
+
+        // Check if it have been 0.8 seconds and the Press Start Text Field is Empty
+        if (isPressStartTextEmpty && timer >= 0.8f)
+        {
+            pressStartText.text = "Press 'X' to Start the Game";
+
+            // Make the isPressStartTextEmpty text to be false
+            isPressStartTextEmpty = false;
+
+            // Make the isPressStartTextThere text to be true
+            isPressStartTextThere = true;
+
+            ResetTimer();
+        }
+
     }
+
+
+    // Method -- Reset the Timer to have a float value of 0.0f
+    private void ResetTimer()
+    {
+        timer = 0.0f;
+    }
+
 
 
 }
