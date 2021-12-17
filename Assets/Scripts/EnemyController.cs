11965667f91d87a242513034e0bca34a8a9ab12b
AH-work000 <70098254+AH-work000@users.noreply.github.com>
2021-12-17 16:23:23 +1100

    Wall-Collider-Rev(1)

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -13,26 +13,57 @@ public class EnemyController : MonoBehaviour
 
     private Transform goblinEnemyTransform;
 
+    // Bool Member Variables
+    private bool hasGoblinMeetWall;
+
+    private bool isGoblinGoingLeft;
+
+    private bool isGoblinClimbingDown;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
         // Initialize the goblinEnemyTransform
         goblinEnemyTransform = gameObject.transform;
 
+        // Initialize all the bool variables as false
+        hasGoblinMeetWall = false;
+
+        isGoblinGoingLeft = false;
+
+        isGoblinClimbingDown = false;
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        DoLerp();
+        if (hasGoblinMeetWall)
+        {
+            // Y Axis Lerp going up
+            DoLerp(0.0f, 1.0f);
+        }
+        else if (isGoblinClimbingDown)
+        {
+            // Y Axis Lerp going down
+            DoLerp(0.0f, -1.0f);
+        }
+        else 
+        {
+            // X Axis Lerp in a left direction
+            DoLerp(-1.0f, 0.0f);
+        }
+
     }
 
 
     // Method: Lerp Movement for the Goblin
-    void DoLerp()
+    void DoLerp(float xAxisMovement, float yAxisMovement)
     {
         // Designate the end position of the goblin enemy
-        Vector3 endPos = new Vector3(goblinEnemyTransform.position.x - 1.0f, goblinEnemyTransform.position.y, goblinEnemyTransform.position.z);
+        Vector3 endPos = new Vector3(goblinEnemyTransform.position.x + xAxisMovement, goblinEnemyTransform.position.y + yAxisMovement);
 
         // Calculate the timeFraction
         float timeFraction = Time.deltaTime / duration;
@@ -43,11 +74,30 @@ public class EnemyController : MonoBehaviour
     }
 
     // Message: OnCollisionEnter to detect when a collider/rigidbody touches this gameObject
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.tag == "Impassable")
         {
-            Debug.Log("Going thru the wall!");
+            // Make the hasGoblinMeetWall bool to be true as the Goblin is at the wall!!!
+            hasGoblinMeetWall = true;
+            isGoblinGoingLeft = false;
+            isGoblinClimbingDown = false;
+        }
+
+        if (collision.tag == "Go Left")
+        {
+            // Make the isGoblinGoingLeft bool to be true as the Goblin have finished dealing with the wall!!!
+            isGoblinGoingLeft = true;
+            hasGoblinMeetWall = false;
+            isGoblinClimbingDown = false;
+        }
+
+        if (collision.tag == "Go Down")
+        {
+            // Make the isGoblinClimbingDown bool to be true as the Goblin is at the edge!
+            isGoblinClimbingDown = true;
+            isGoblinGoingLeft = false;
+            hasGoblinMeetWall = false;
         }
     }
 }
