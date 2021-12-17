babfda1311d282516ee10a08f6e9176d10bdcfa3
AH-work000 <70098254+AH-work000@users.noreply.github.com>
2021-12-17 12:38:11 +1100

    Enemy-Movement-Added

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    // Member Variables
+    [SerializeField]
+    private float duration;
+
+    /* [SerializeField]
+    private float lerpDirection; */
+
+    private Transform goblinEnemyTransform;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Initialize the goblinEnemyTransform
+        goblinEnemyTransform = gameObject.transform;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        DoLerp();
     }
+
+
+    // Method: Lerp Movement for the Goblin
+    void DoLerp()
+    {
+        // Designate the end position of the goblin enemy
+        Vector3 endPos = new Vector3(goblinEnemyTransform.position.x - 1.0f, goblinEnemyTransform.position.y, goblinEnemyTransform.position.z);
+
+        Debug.Log("EndPos: " + endPos);
+
+        // Calculate the timeFraction
+        float timeFraction = Time.deltaTime / duration;
+
+        // Perform Lerp using Vector3.lerp Unity function
+        goblinEnemyTransform.position = Vector3.Lerp(goblinEnemyTransform.position, endPos, timeFraction);
+
+        Debug.Log("Final Position: " + goblinEnemyTransform.position);
+    }
+
 }
